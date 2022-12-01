237dbb73f78d234586ef22a78de90c44d02fd858
Cornell Castelino <ccastelino12@gmail.com>
2022-12-01 13:36:50 +0000

    inputs

--- importantFiles/training.py
+++ importantFiles/training.py
@@ -18,22 +18,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 df = pd.read_csv('importantFiles\Elon_class.csv')
 # df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
 
-x = df.iloc[:,2]  # construct a matrix containing tweets
-y = df.iloc[:,3]  # construct a matrix containing -1, 0 or 1
+x = df['Tweet']  # construct a matrix containing tweets
+y = df['Class']  # construct a matrix containing -1, 0 or 1
 
 # a text featuring 
 
-# reviews = pd.Series(x)
-# reviews.str.cat(sep=' ')
-# #function to split text into word
-# tokens = word_tokenize(reviews)
-# vocabulary = set(tokens)
-# print(len(vocabulary))
-# frequency_dist = nltk.FreqDist(tokens)
-# sorted(frequency_dist,key=frequency_dist.__getitem__, reverse=True)[0:50]
-# # remove the stop words to cleanup the text
-# stop_words = set(stopwords.words('english'))            
-# tokens = [w for w in tokens if not w in stop_words]
+reviews = pd.Series(x)
+reviews.str.cat(sep=' ')
+#function to split text into word
+tokens = word_tokenize(reviews)
+vocabulary = set(tokens)
+print(len(vocabulary))
+frequency_dist = nltk.FreqDist(tokens)
+sorted(frequency_dist,key=frequency_dist.__getitem__, reverse=True)[0:50]
+# remove the stop words to cleanup the text
+stop_words = set(stopwords.words('english'))            
+tokens = [w for w in tokens if not w in stop_words]
 
 # text featuring ends
 print(x)
