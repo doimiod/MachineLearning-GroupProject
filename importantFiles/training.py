552c20c2f23c2b36ad4eb676958716ba7ea547cf
doimiod <doim@tcd.ie>
2022-12-01 17:44:08 +0000

    cross_val

--- importantFiles/training.py
+++ importantFiles/training.py
@@ -20,14 +20,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 from sklearn.metrics import f1_score
+from sklearn.preprocessing import PolynomialFeatures
 
 # #CAREFUL only for deprecation of datetime64
 import warnings
 warnings.filterwarnings("ignore")
 # #CAREFUL
 
-df = pd.read_csv('importantFiles\Elon_class.csv')
-# df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
+# df = pd.read_csv('importantFiles\Elon_class.csv')
+df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
 
 x = df['Tweet']  # construct a matrix containing tweets
 y = df['Class']  # construct a matrix containing -1, 0 or 1
@@ -92,10 +93,11 @@ xTrain = vectorizer.fit_transform(xTrain)
 xTest = vectorizer.transform(xTest)
 test  = vectorizer.get_feature_names_out()
 dfs = pd.DataFrame(test, columns=['Features'])
-dfs.to_csv("importantFiles/test.csv", encoding='utf_8_sig')
+dfs.to_csv("test.csv", encoding='utf_8_sig')
+# dfs.to_csv("importantFiles/test.csv", encoding='utf_8_sig')
 print(test)
 
-def logisticRegression(C,xTrain, yTrain, xTest, yTest): # train data by logistic Regression
+def logisticRegression(C, xTrain, yTrain, xTest, yTest): # train data by logistic Regression
     print("\nLogistic Regression, with C = {}__________________________________".format(C))
     model = LogisticRegression(C=C,penalty = "l2")
     model.fit(xTrain, yTrain)                       # train data
@@ -107,18 +109,25 @@ def logisticRegression(C,xTrain, yTrain, xTest, yTest): # train data by logistic
     ypred = ypred.reshape(-1,1)            # make a tidy array of prediction data which contains values, -1, 0 or 1
     print(classification_report(yTest, ypred))
     print(confusion_matrix(yTest,ypred))
-    temp=[]
+
     mean_error=[]
     std_error = []
-    kf = KFold(n_splits=5)
-    for train, test in kf.split(xTrain):
-        model.fit(xTrain[train,:], yTrain[train])
-        ypred = model.predict(xTrain[test,:])
-        temp.append(f1_score(yTrain[test],ypred))
-        Xnew = xTrain[test,:]
-    mean_error.append(np.array(temp).mean())
-    std_error.append(np.array(temp).std())
-    plot(mean_error, std_error)
+    C_values = [0.1, 1, 10, 100]
+    for Ci in C_values:
+        # xTrain = PolynomialFeatures(degree = Ci).fit_transform(x)
+        model = LogisticRegression(penalty = "l2",C = Ci, solver="lbfgs")
+        #5 fold CV
+        temp=[]
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(xTrain):        
+            model.fit(xTrain[train], yTrain[train])
+            ypred = model.predict(xTrain[test])
+            temp.append(f1_score(yTrain[test],ypred,average = "micro"))
+            Xnew = xTrain[test,:]
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plot(C_values, mean_error, std_error)
+        # plt.errorbar(polyDegree, mean_error, yerr=std_error, ecolor ="red", marker = "o", ms=3)
 
 
 def linear_SVC (C, xTrain, yTrain, xTest, yTest):     # train data by SVC
@@ -135,6 +144,22 @@ def linear_SVC (C, xTrain, yTrain, xTest, yTest):     # train data by SVC
     ypred = ypred.reshape(-1,1)            # make a tidy array of prediction data which contains values, -1, 0 or 1
     print(classification_report(yTest, ypred))
     print(confusion_matrix(yTest,ypred))
+    mean_error=[]
+    std_error = []
+    C_values = [0.1, 1, 10, 100]
+    for Ci in C_values:
+        model = LinearSVC(C=C, penalty= "l2").fit(xTrain, yTrain)
+        #5 fold CV
+        temp=[]
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(xTrain):        
+            model.fit(xTrain[train], yTrain[train])
+            ypred = model.predict(xTrain[test])
+            temp.append(f1_score(yTrain[test],ypred,average = "micro"))
+            Xnew = xTrain[test,:]
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plot(C_values, mean_error, std_error)
 
 def baseline_mostFrequent(xTrain, yTrain, xTest, yTest):
     print("\nbaseline mostfrequency classifier__________________________________")
@@ -147,8 +172,8 @@ def baseline_mostFrequent(xTrain, yTrain, xTest, yTest):
     print(confusion_matrix(yTest,ypred))
 
 
-def plot(mean_error, std_error):
-    plt.errorbar(mean_error, yerr=std_error, ecolor ="red", marker = "o", ms=3)
+def plot(c, mean_error, std_error):
+    plt.errorbar(c, mean_error, yerr=std_error, ecolor ="red", marker = "o", ms=3)
     # plt.xlabel("Degree of polynomial")
     plt.ylabel("f1 score")
     plt.title("Logistic regression")
@@ -165,15 +190,15 @@ def plot(mean_error, std_error):
 #     model = LogisticRegression(penalty = "l2",C = 1, solver="lbfgs")
 
 #     #5 fold CV
-    # temp=[]
-    # kf = KFold(n_splits=5)
-    # for train, test in kf.split(Xtrain):
-    #     model.fit(Xtrain[train,:], ytrain[train])
-    #     ypred = model.predict(Xtrain[test,:])
-    #     temp.append(f1_score(ytrain[test],ypred))
-    #     Xnew = Xtrain[test,:]
-    # mean_error.append(np.array(temp).mean())
-    # std_error.append(np.array(temp).std())
+#     temp=[]
+#     kf = KFold(n_splits=5)
+#     for train, test in kf.split(Xtrain):
+#         model.fit(Xtrain[train,:], ytrain[train])
+#         ypred = model.predict(Xtrain[test,:])
+#         temp.append(f1_score(ytrain[test],ypred))
+#         Xnew = Xtrain[test,:]
+#     mean_error.append(np.array(temp).mean())
+#     std_error.append(np.array(temp).std())
 
 
 # #CHOSEN ORDER OF POLYNOMIAL
@@ -186,7 +211,7 @@ def plot(mean_error, std_error):
 # plt.show()
 
 
-logisticRegression(0.1,xTrain, yTrain, xTest, yTest)
+logisticRegression(0.1, xTrain, yTrain, xTest, yTest)
 # linear_SVC (0, xTrain, yTrain)
 linear_SVC (0.01, xTrain, yTrain,  xTest, yTest)
 linear_SVC (10, xTrain, yTrain,  xTest, yTest)
