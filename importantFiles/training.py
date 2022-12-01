c6018227ffca76963ce18fee928f8cdd87793c06
Cornell Castelino <ccastelino12@gmail.com>
2022-12-01 14:31:04 +0000

    updates

--- importantFiles/training.py
+++ importantFiles/training.py
@@ -18,11 +18,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# df = pd.read_csv('importantFiles\Elon_class.csv')
-df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
+df = pd.read_csv('importantFiles\Elon_class.csv')
+# df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
 
 x = df['Tweet']  # construct a matrix containing tweets
-y = df['Class']  # construct a matrix containing -1, 0 or 1
+y = df['Class'] # construct a matrix containing -1, 0 or 1
 
 # # a text featuring 
 # tweets = df.Tweet.str.cat(sep=' ')
@@ -51,21 +51,22 @@ tokens = [w for w in tokens if not w in stop_words]
 
 print(tokens)
 # text featuring ends
-print(x)
 
-xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.8) # split the data for training and testing.
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2) # split the data for training and testing.
+print(type(xTrain))
 xTest = np.array(xTest) #make an array of x test data
 
 vectorizer = TfidfVectorizer()
-xTrain = vectorizer.fit_transform(xTrain)
-xTest = vectorizer.transform(xTest)
+# x = v.fit_transform(df['Review'].values.astype('U'))
+xTrain = vectorizer.fit_transform(xTrain.astype('U').values)
+xTest = vectorizer.transform(xTest.astype('U'))
 
 def logisticRegression(xTrain, yTrain): # train data by logistic Regression
     model = LogisticRegression()
     model.fit(xTrain, yTrain)                       # train data
     print("slope = ", model.coef_)                       # get a slope here
     print("intercept = ", model.intercept_)              # get an intercept here
-    print("train score = ", format(model.score(xTrain, yTrain)))
+    print("mean accuracy = ", format(model.score(xTrain, yTrain)))
     print('\n')
     predData = np.array(model.predict(xTest))   
     predData = predData.reshape(-1,1)            # make a tidy array of prediction data which contains values, -1, 0 or 1
@@ -76,7 +77,7 @@ def linear_SVC (c, xTrain, yTrain):     # train data by SVC
     print("when C =", c)                                                    
     print("slope = ", model.coef_)                                          # get a slope here
     print("intercept = ", model.intercept_)                                 # get an intercept here
-    print("train score = ", format(model.score(xTrain, yTrain)))
+    print("mean accuracy = ", format(model.score(xTrain, yTrain)))
     print('\n')
     predData = np.array(model.predict(xTest))   
     predData = predData.reshape(-1,1)            # make a tidy array of prediction data which contains values, -1, 0 or 1
