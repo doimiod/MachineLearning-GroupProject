663d48770d0b76d4366b2f8823f9ab98a826e9fb
Cornell Castelino <ccastelino12@gmail.com>
2022-12-02 18:22:35 +0000

    Big update

--- importantFiles/preprocessing.py
+++ importantFiles/preprocessing.py
@@ -16,7 +16,7 @@ warnings.filterwarnings("ignore")
 # #CAREFUL
 
 #Initialization
-threshold = 0.05
+threshold = 0.02
 Elon_class_avail = True
 Elon_data_avail = True
 update_tweet = False
@@ -24,7 +24,7 @@ update_class = False
 Elon_class = []
 Elon_data = []
 try:
-    Elon_data = pd.read_csv("{}ElonsTweets.csv".format(Address))
+    Elon_data = pd.read_csv("{}ElonsTweets1.csv".format(Address))
 except:
     Elon_data_avail = False
 try:
@@ -36,7 +36,7 @@ try:
 except: 
     x=1
 try:
-    Elon_class = pd.read_csv("{}Elon_class.csv".format(Address))
+    Elon_class = pd.read_csv("{}Elon_class1.csv".format(Address))
 except:
     Elon_avail = False
 
@@ -48,7 +48,7 @@ if(Elon_data_avail):
 #________________GATHERING___TWEETS_______________________________
 if(not Elon_data_avail or update_tweet):
     print(" Gathering Tweets...")
-    query = "(from:elonmusk) since:2010-06-29 -filter:replies"
+    query = "(from:elonmusk) since:2010-06-29"
     tweets = []
     limit = 100000000000
 
@@ -73,7 +73,7 @@ if(not Elon_data_avail or update_tweet):
 
     print(df)
 
-    df.to_csv("{}ElonsTweets.csv".format(Address), encoding='utf_8_sig')
+    df.to_csv("{}ElonsTweets1.csv".format(Address), encoding='utf_8_sig')
     E_date = df['Date']
     E_size = len(E_date)
 else:
@@ -87,14 +87,14 @@ if (val == 'y'):
     update_class = True
 
 #starting point
-#class increases: 1
-#class decreses: -1
+#class increases: 2
+#class decreses: -2
 #class undecided: 0
 #classes for stocks between dates. need to assign the class to the tweets at the relevant dates.
 
 #
 if(not Elon_class_avail or update_class):
-    
+    Elon_data = pd.read_csv("{}ElonsTweets1.csv".format(Address))
     #convert dict to array to datetime64 to only yyyy-mm-hh
     E_date = Elon_data['Date']
     E_date = np.array(E_date, dtype=np.datetime64)
@@ -103,7 +103,7 @@ if(not Elon_class_avail or update_class):
     E_size = len(E_date)
 
     # initial_date = E_date[E_size-1]
-    
+    Elon_class = []
     for i in range(0, E_size):
         Elon_class.append([E_date[i], Elon_data['Tweet'][i],"",""])
 
@@ -134,9 +134,9 @@ if(not Elon_class_avail or update_class):
             #try to find a valid close price, if less than 2 dates found, decrement count
             try:
                 if(data['Close'][size-1]-data['Close'][0] > data['Close'][0]*threshold):
-                    class_val = 1
+                    class_val = 2
                 elif(data['Close'][size-1]-data['Close'][0] < -data['Close'][0]*threshold):
-                    class_val = -1
+                    class_val = -2
                 else:
                     class_val = 0
                 class_found= True
@@ -157,7 +157,7 @@ if(not Elon_class_avail or update_class):
     #store Elon_data
     columns = ['Date','Tweet', 'Class', 'Close_price']
     df = pd.DataFrame(Elon_class, columns=columns)
-    df.to_csv("{}Elon_class.csv".format(Address), encoding='utf_8_sig')
+    df.to_csv("{}Elon_class1.csv".format(Address), encoding='utf_8_sig')
 else:
     if(Elon_class_avail):
         print("class available")
@@ -174,15 +174,15 @@ else:
             break
 
         if(Elon_class['Close_price'][i+j]-Elon_class['Close_price'][i] > Elon_class['Close_price'][i]*threshold):
-            class_val = 1
+            class_val = 2
         elif(Elon_class['Close_price'][i+j]-Elon_class['Close_price'][i] < -Elon_class['Close_price'][i]*threshold):
-            class_val = -1
+            class_val = -2
         else:
             class_val = 0
         Elon_class['Class'][i] = class_val
-    Elon_class.to_csv("{}Elon_class.csv".format(Address), encoding='utf_8_sig')
+    Elon_class.to_csv("{}Elon_class1.csv".format(Address), encoding='utf_8_sig')
 
-Elon_class = pd.read_csv("{}Elon_class.csv".format(Address))
+Elon_class = pd.read_csv("{}Elon_class1.csv".format(Address))
 
 #remove URL
 import re
@@ -190,19 +190,20 @@ import re
 for i in range(0,len(Elon_class)):
     try:
         Elon_class['Tweet'][i] = re.sub(r'http\S+', '', Elon_class['Tweet'][i])
+        Elon_class['Tweet'][i] = re.sub(r'@', '', Elon_class['Tweet'][i])
     except:
         x=1
 
-Elon_class.to_csv("{}Elon_class.csv".format(Address), encoding='utf_8_sig',)
+Elon_class.to_csv("{}Elon_class1.csv".format(Address), encoding='utf_8_sig',)
 
 #sum of individual classes
 class1 = 0
 class1p = 0
 class_n = 0
 for i in range(0,len(Elon_class)-1):
-    if(Elon_class['Class'][i] == 1):
+    if(Elon_class['Class'][i] == 2):
         class1 = class1+1
-    if(Elon_class['Class'][i] == -1):
+    if(Elon_class['Class'][i] == -2):
         class1p = class1p+1
     if(Elon_class['Class'][i] == 0):
         class_n = class_n+1
--- importantFiles/training.py
+++ importantFiles/training.py
@@ -21,29 +21,38 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import PolynomialFeatures
+from yellowbrick.classifier import ROCAUC
+
+
 
 # #CAREFUL only for deprecation of datetime64
 import warnings
 warnings.filterwarnings("ignore")
 # #CAREFUL
 
-df = pd.read_csv('importantFiles\Elon_class.csv')
+df = pd.read_csv('importantFiles\Elon_class1.csv')
 # df = pd.read_csv('/Users/doimasanari/Desktop/MachineLearning-GroupProject/importantFiles/Elon_class.csv')
 
 x = df['Tweet']  # construct a matrix containing tweets
 y = df['Class']  # construct a matrix containing -1, 0 or 1
 date = df['Date'] # construct a matrix containing the date
 
-Tweets = df['Tweet'].str.cat(sep=' ')
-#function to split text into word
-tokens = word_tokenize(Tweets)
-vocabulary = set(tokens)
-print(len(vocabulary))
-frequency_dist = nltk.FreqDist(tokens)
-print(sorted(frequency_dist,key=frequency_dist.__getitem__, reverse=True)[0:50])
-stop_words = set(stopwords.words('english'))
-tokens = [w for w in tokens if not w in stop_words]
-frequency_dist2 = nltk.FreqDist(tokens)
+# Tweets = df['Tweet'].str.cat(sep=' ')
+# #function to split text into word
+# tokens = word_tokenize(Tweets)
+# vocabulary = set(tokens)
+# print(len(vocabulary))
+# frequency_dist = nltk.FreqDist(tokens)
+# print(sorted(frequency_dist,key=frequency_dist.__getitem__, reverse=True)[0:50])
+# stop_words = set(stopwords.words('english'))
+# tokens = [w for w in tokens if not w in stop_words]
+# frequency_dist2 = nltk.FreqDist(tokens)
+
+# from wordcloud import WordCloud
+# wordcloud = WordCloud().generate_from_frequencies(frequency_dist2)
+# plt.imshow(wordcloud)
+# plt.axis("off")
+# plt.show()
 
 print(x)
 
@@ -53,7 +62,7 @@ xTrain = []
 xTest = []
 yTrain = []
 yTest = []
-split = 0.95
+split = 0.85
 
 for i in range(len(x)):
 
@@ -74,15 +83,36 @@ yTest = np.array(yTest) #make an array of y test data
 # print(xTrain)
 # print(xTest)
 
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer( lowercase=False, ngram_range=(2,2))
 # x = v.fit_transform(df['Review'].values.astype('U'))
 xTrain = vectorizer.fit_transform(xTrain)
 xTest = vectorizer.transform(xTest)
 test  = vectorizer.get_feature_names_out()
 dfs = pd.DataFrame(test, columns=['Features'])
-dfs.to_csv("test.csv", encoding='utf_8_sig')
+# dfs.to_csv("test.csv", encoding='utf_8_sig')
 # dfs.to_csv("importantFiles/test.csv", encoding='utf_8_sig')
-print(test)
+print(test[0:50])
+coef=vectorizer.idf_
+coef_feat = vectorizer.get_feature_names()
+coef,coef_feat = (list(t) for t in zip(*sorted(zip(coef, coef_feat))))
+size = len(coef)
+# print(coef[0:50])
+# print(coef[size-50:size])
+# print (dict(zip(vectorizer.get_feature_names(), vectorizer.idf_)))
+
+def plot_ROC_curve(model, xtrain, ytrain, xtest, ytest):
+
+    # Creating visualization with the readable labels
+    visualizer = ROCAUC(model, encoder={2: 'Stock increase', 
+                                        0: 'nothing happens', 
+                                        -2: 'Stock decreaess'})
+                                        
+    # Fitting to the training data first then scoring with the test data                                    
+    visualizer.fit(xtrain, ytrain)
+    visualizer.score(xtest, ytest)
+    visualizer.show()
+    
+    return visualizer
 
 def logisticRegression_CV(C, xTrain, yTrain, xTest, yTest): # train data by logistic Regression
     print("\nLogistic Regression, with C = {}__________________________________".format(C))
@@ -90,12 +120,12 @@ def logisticRegression_CV(C, xTrain, yTrain, xTest, yTest): # train data by logi
     #Cross validation of LR
     mean_error=[]
     std_error = []
-    count = 0
+    count = len(C)
     for Ci in C:
         print("progress: {}".format(count))
-        count=count+1
+        count=count-1
         # xTrain = PolynomialFeatures(degree = Ci).fit_transform(x)
-        model1 = LogisticRegression(penalty = "l2",C = Ci,multi_class='ovr')
+        model1 = LogisticRegression(penalty = "l2",C = Ci,multi_class='ovr',class_weight='balanced')
         #5 fold CV
         temp=[] 
         kf = KFold(n_splits=5)
@@ -113,11 +143,14 @@ def logisticRegression_CV(C, xTrain, yTrain, xTest, yTest): # train data by logi
         mean_error.append(np.array(temp).mean())
         std_error.append(np.array(temp).std())
     
-
+    model1 = LogisticRegression(penalty = "l2",C = 10**5,multi_class='ovr',class_weight='balanced')
     model1.fit(xTrain,yTrain)
     ypred = model1.predict(xTest)
+
+
     print(classification_report(yTest, ypred))
     print(confusion_matrix(yTest,ypred))
+    plot_ROC_curve(model1,xTrain, yTrain, xTest, yTest)
     plot(C, mean_error, std_error, True)
 
 
@@ -127,12 +160,12 @@ def linear_SVC_CV (C, xTrain, yTrain, xTest, yTest):     # train data by SVC
     #Cross validation of LR
     mean_error=[]
     std_error = []
-    count = 1
+    count = len(C)
     for Ci in C:
         print("progress: {}".format(count))
-        count=count+1
+        count=count-1
         # xTrain = PolynomialFeatures(degree = Ci).fit_transform(x)
-        model1 = LinearSVC(penalty = "l2",C = Ci, multi_class='ovr')
+        model1 = LinearSVC(penalty = "l2",C = Ci, multi_class='ovr',class_weight='balanced')
      
         #5 fold CV
         temp=[] 
@@ -151,12 +184,13 @@ def linear_SVC_CV (C, xTrain, yTrain, xTest, yTest):     # train data by SVC
             temp.append(f1_score(yTrain[test],ypred1,average = "micro"))
         mean_error.append(np.array(temp).mean())
         std_error.append(np.array(temp).std())
-    
 
+    model1 = LinearSVC(penalty = "l2",C = 10**5, multi_class='ovr',class_weight='balanced')
     model1.fit(xTrain,yTrain)
     ypred = np.array(model1.predict(xTest))
     print(classification_report(yTest, ypred))
     print(confusion_matrix(yTest,ypred))
+    plot_ROC_curve(model1,xTrain, yTrain, xTest, yTest)
     plot(C, mean_error, std_error, False)
 
 def baseline_mostFrequent(xTrain, yTrain, xTest, yTest):
@@ -168,6 +202,7 @@ def baseline_mostFrequent(xTrain, yTrain, xTest, yTest):
     ypred = ypred.reshape(-1,1)            # make a tidy array of prediction data which contains values, -1, 0 or 1
     print(classification_report(yTest, ypred))
     print(confusion_matrix(yTest,ypred))
+    plot_ROC_curve(dummy,xTrain, yTrain, xTest, yTest)
 
 
 def plot(c, mean_error, std_error, isLogistic):
@@ -181,6 +216,37 @@ def plot(c, mean_error, std_error, isLogistic):
         plt.title("Cross Validation in LinearSVC, C = " + str(c))
     plt.show()
 
+# def plot_top_features(classifier):
+#     model_coefs = pd.DataFrame(classifier.coef_)
+#     coefs_df = model_coefs.T
+#     feature_name_list =  
+#     # list w/ eng'd features & tf-idf n-grams
+#     all_feat_names = []
+#     for i in feature_name_list:
+#         all_feat_names.append(i)
+#     for i in tfidf.get_feature_names():
+#         all_feat_names.append(i)
+#     # creating column for feat names
+#     coefs_df['feats'] = all_feat_names
+#     coefs_df.set_index('feats', inplace=True)
+#     coefs_df['feats'] = all_feat_names
+#     coefs_df.set_index('feats', inplace=True)
+#     # plot non-cb
+#     coefs_df[0].sort_values(ascending=True).head(20).plot(kind='bar')
+#     plt.title("SVM: Top 20 Non-Clickbait Coefs")
+#     # plt.title("LogReg: Top 20 Non-Clickbait Coefs")
+#     plt.xlabel("features")
+#     plt.ylabel("coef value")
+#     plt.xticks(rotation=55)
+#     plt.show()
+#     # plot CB classification
+#     coefs_df[0].sort_values(ascending=False).head(20).plot(kind='bar', color='orange')
+#     plt.title("SVM: Top 20 Clickbait Coefs")
+#     # plt.title("LogReg: Top 20 Clickbait Coefs")
+#     plt.xlabel("features")
+#     plt.ylabel("coef value")
+    # plt.xticks(rotation=55)
+    # plt.show()
 
 
 # mean_error=[]
@@ -206,9 +272,9 @@ def plot(c, mean_error, std_error, isLogistic):
 
 
 
-
-logisticRegression_CV([0.1,1,10,100], xTrain, yTrain, xTest, yTest)
-# linear_SVC (0, xTrain, yTrain)
-linear_SVC_CV ([0.1,1,10,100], xTrain, yTrain,  xTest, yTest)
-
 baseline_mostFrequent(xTrain, yTrain,  xTest, yTest)
+
+logisticRegression_CV([10**-2,10**-1,10**0,10,50,10**5], xTrain, yTrain, xTest, yTest)
+# linear_SVC (0, xTrain, yTrain)
+linear_SVC_CV ([10**-2,10**-1,10**0,10,50,10**5], xTrain, yTrain,  xTest, yTest)
+
